e800988fde09236e12f3647766dc4e800faf2474
LionKing123412 <bbgamingthe11@gmail.com>
2026-06-03 14:55:40 -0400

    Update reinstall.lua

--- reinstall.lua
+++ reinstall.lua
@@ -1,7 +1,6 @@
 local folderToClean = "newvape"
 local folderToKeep = "profiles"
-local reinstallUrl = "https://raw.githubusercontent.com/poopparty/poopparty/main/NewMainScript.lua"
-
+local reinstallUrl = "https://raw.githubusercontent.com/LionKing123412/LionV5/main/NewMainScript.lua"
 local function deleteRecursive(path, keepPath)
     if path == keepPath then return end
     if isfolder and isfolder(path) then
@@ -15,23 +14,19 @@ local function deleteRecursive(path, keepPath)
         delfile(path)
     end
 end
-
 if not isfolder(folderToClean) then
     print("folder '" .. folderToClean .. "' not found.")
     return
 end
-
 local keepFullPath = folderToClean .. "/" .. folderToKeep
 if not isfolder(keepFullPath) then
     print("warning nigga: '" .. keepFullPath .. "' missing  - everything boutta get deleted gang")
 end
-
 for _, item in ipairs(listfiles(folderToClean)) do
     if item ~= keepFullPath then
         deleteRecursive(item, keepFullPath)
     end
 end
-
 print("loading aerov4...")
 task.wait(1) --no no crash dabo
 loadstring(game:HttpGet(reinstallUrl, true))()
