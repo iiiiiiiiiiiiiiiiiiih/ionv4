2e0045f0fa7e479beb8d657fd01bd508e6e42a1f
LionKing123412 <bbgamingthe11@gmail.com>
2026-06-03 15:19:51 -0400

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -562,16 +562,9 @@ run(function()
 		end
 	end
 
-	function whitelist:update(first)
+function whitelist:update(first)
 		local suc = pcall(function()
-			local _, subbed = pcall(function()
-				return game:HttpGet('https://github.com/poopparty/whitelistcheck')
-			end)
-			local commit = subbed:find('currentOid')
-			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..commit..'/PlayerWhitelist.json', true)
-
+			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/main/PlayerWhitelist.json', true)
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
