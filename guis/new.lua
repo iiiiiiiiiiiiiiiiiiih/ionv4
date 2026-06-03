88a3945c7ea66768a8189fac357b92956748b534
LionKing123412 <bbgamingthe11@gmail.com>
2026-06-03 15:29:46 -0400

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -1405,7 +1405,7 @@ local function downloadFile(path, func)
 	if not isfile(path) then
 		createDownloader(path)
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('LionV5/profiles/commit.txt')..'/'..select(1, path:gsub('LionV5/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..readfile('LionV5/profiles/commit.txt')..'/'..select(1, path:gsub('LionV5/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -8236,7 +8236,7 @@ general:CreateButton({
 		if shared.VapeDeveloper then
 			loadstring(readfile('LionV5/loader.lua'), 'loader')()
 		else
-			loadstring(game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
+			loadstring(game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
 		end
 	end,
 	Tooltip = 'This will set your profile to the default settings of Vape'
@@ -8255,7 +8255,7 @@ general:CreateButton({
 		if shared.VapeDeveloper then
 			loadstring(readfile('LionV5/loader.lua'), 'loader')()
 		else
-			loadstring(game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
+			loadstring(game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
 		end
 	end,
 	Tooltip = 'Reloads vape for debugging purposes'
@@ -8417,7 +8417,7 @@ guipane:CreateDropdown({
 			if shared.VapeDeveloper then
 				loadstring(readfile('LionV5/loader.lua'), 'loader')()
 			else
-				loadstring(game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
+				loadstring(game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..readfile('LionV5/profiles/commit.txt')..'/loader.lua', true))()
 			end
 		end
 	end,
