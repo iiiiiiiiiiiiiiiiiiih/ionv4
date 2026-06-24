a6154cb7fe8240ce5b8ed28036aba5b5caab0d06
iiiiiiiiiiiiiiiiiiih <josephjosafa691@gmail.com>
2026-06-23 22:59:16 -0400

    Update loader.lua for ionv4 integration

--- loader.lua
+++ loader.lua
@@ -10,13 +10,13 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/LionKing123412/LionV5/'..readfile('LionV5/profiles/commit.txt')..'/'..select(1, path:gsub('LionV5/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/iiiiiiiiiiiiiiiiiiih/ionv4/'..readfile('ionv4/profiles/commit.txt')..'/'..select(1, path:gsub('ionv4/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--ionv4\n'..res
 		end
 		writefile(path, res)
 	end
@@ -26,12 +26,12 @@ local function wipeFolder(path)
 	if not isfolder(path) then return end
 	for _, file in listfiles(path) do
 		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+		if isfile(file) and select(1, readfile(file):find('--ionv4')) == 1 then
 			delfile(file)
 		end
 	end
 end
-for _, folder in {'LionV5', 'LionV5/games', 'LionV5/profiles', 'LionV5/assets', 'LionV5/libraries', 'LionV5/guis'} do
+for _, folder in {'ionv4', 'ionv4/games', 'ionv4/profiles', 'ionv4/assets', 'ionv4/libraries', 'ionv4/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
@@ -39,7 +39,7 @@ end
 if not shared.VapeDeveloper then
 	local commit = 'main'
 	local ok, res = pcall(function()
-		return game:HttpGet('https://api.github.com/repos/LionKing123412/LionV5/commits/main', true)
+		return game:HttpGet('https://api.github.com/repos/iiiiiiiiiiiiiiiiiiih/ionv4/commits/main', true)
 	end)
 	if ok and res then
 		local h = res:match('"sha":"([a-f0-9]+)"')
@@ -47,15 +47,15 @@ if not shared.VapeDeveloper then
 			commit = h
 		end
 	end
-	if commit == 'main' or (isfile('LionV5/profiles/commit.txt') and readfile('LionV5/profiles/commit.txt') or '') ~= commit then
-    if isfile('LionV5/main.lua') then delfile('LionV5/main.lua') end
-    	wipeFolder('LionV5')
-    	wipeFolder('LionV5/games')
-    	wipeFolder('LionV5/guis')
-    	wipeFolder('LionV5/libraries')
+	if commit == 'main' or (isfile('ionv4/profiles/commit.txt') and readfile('ionv4/profiles/commit.txt') or '') ~= commit then
+    if isfile('ionv4/main.lua') then delfile('ionv4/main.lua') end
+    	wipeFolder('ionv4')
+    	wipeFolder('ionv4/games')
+    	wipeFolder('ionv4/guis')
+    	wipeFolder('ionv4/libraries')
 	end
-	writefile('LionV5/profiles/commit.txt', commit)
+	writefile('ionv4/profiles/commit.txt', commit)
 end
-return loadstring(downloadFile('LionV5/main.lua'), 'main')({
+return loadstring(downloadFile('ionv4/main.lua'), 'main')({
     Username = shared.ValidatedUsername
 })
